973fb287b4fed1a9f2aab368f347ff1c6b3bcc16
Aditya Kumar <67699932+mradityaroy@users.noreply.github.com>
2022-03-29 12:02:24 +0530

    Add files via upload
    I created 3(three) different functions for different purposes named action_perform,login And register
    i also declared 2(two) global hash (key-value pair) named login_detail and all_data which is responsible for storing login related data (user id  and password) and vaccine booking related data such as name,place,mobile number,age,dosage,etc respectively in terms of key-value pair.
    
    action_perform function is responsible for showing option and redirecting to another function whatever user choose
    In action_perform function i created one local hash for storing option for what action user wants in term of key-value pair after that used foreach loop for retrieving and showing data from local hash named choice then after apply condition by if elsif for if user press 1 then if condition true and login function called but if condition false then it go for next line annd execute elsif line check the condition if true then register function called.
    
    If register function called then following actions happens-
    Take input from user such as first name,last name,date of birth, mobile number, country name,place,age, dosage and stored in local hash named data and then all data of local hash (data) stored in global hash (all_data) by key -value pair key is his/her id number value is whatever data stored in local hash amd control goes back to from where function called and next line executed called login function.
    
    If login function called then following actions happens-
    user have to enter user id and password if condition check their data from global hash named login_detail if their data exists it go for another nested if condition that condition check if user entered data matches from existing record or not if match then that user logged in and see their booking by choosing right options from displayed options but if doesn't match it displayed invalid credentials and control goes again back to login function.

---
+++ vaccine_booking.pl
@@ -0,0 +1,126 @@
+#!/usr/bin/perl
+%login_detail=(1234=>1234);
+my %all_data;sub register{
+	
+	my %data;
+	
+	print "Enter Your ID No.: ";   
+	$user_id=<STDIN>;
+	chomp $user_id;
+	
+	print "Enter password: ";
+	$password=<STDIN>;
+	
+	$login_detail{$user_id}=$password;
+
+	print "Enter First Name: ";
+	$user_first=<STDIN>;
+	chomp $user_first;
+	$data{'first_name'}=$user_first;
+	
+	print "Enter Last Name: ";
+	$user_last=<STDIN>;
+	chomp $user_last;
+	$data{'last_name'}=$user_last;
+	
+	print "Enter Mobile No.: ";
+	$user_mob=<STDIN>;
+	chomp $user_mob;
+	$data{'mob'}=$user_mob;
+	
+	print "Enter Date of Birth: ";
+	$user_dob=<STDIN>;
+	chomp $user_dob;
+	$data{'dob'}=$user_dob;
+	
+	print "Enter Country: ";
+	$user_country=<STDIN>;
+	chomp $user_country;
+	$data{'country'}=$user_country;
+	
+	print "Enter Place: ";
+	$user_place=<STDIN>;
+	chomp $user_place;
+	$data{'place'}=$user_place;
+	
+	print "Enter Age: ";
+	$user_age=<STDIN>;
+	chomp $user_place;
+	$data{'age'}=$user_age;
+	
+	print "Enter Dosage: ";
+	$user_dosage=<STDIN>;
+	chomp $user_dosage;
+	$data{'dosage'}=$user_dosage;
+	
+	print "$data{'first_name'}\n";
+	print "Vaccine Booked Successfully for Dosage $user_dosage \n";
+	$all_data{$user_id}=\%data;
+}       sub login{
+	print "Enter Your ID No.: ";
+	$user_id=<STDIN>;
+	chomp $user_id;
+	print "Enter password: ";
+	$password=<STDIN>;
+	chomp $password;
+	
+        if(exists($login_detail{$user_id}))
+        {
+            if($login_detail{$user_id}==$password)
+            {
+                print "---------WELCOME---------\n";
+                print "press 1 for View Booking\n";
+                print "press 2 for logout\n";
+         
+                while(1)
+                {
+                    $action=<STDIN>;
+                    if($action==1)
+                    {
+                        foreach $vals ($all_data{$user_id})
+                        {
+			    print "$vals :$all_data{$user_id}{$vals}";
+			}
+                    }elsif($action==2){
+                       action_perform();
+                    }else{
+                        print "Invaild input";
+                        login();
+                    }
+                }
+            }else
+            {
+                print "Invaild Credentials!\n";
+            }
+        }else
+        {
+        	print "We search everywhere in our system but unfortunately username not found please register with us first\n";
+        	action_perform();
+        }
+
+}
+
+sub action_perform{   
+    %choice = (1=>'press 1 for login',2=>'press 2 for register: ');
+    print "You have already registerd with us? \n";
+    print "Then press 1 \n If you are new user then register with us by clicking 2\n";
+    print "=========================== \n";
+    foreach $word (%choice)
+    {
+        
+        print "$choice{$word}\n";
+    }
+    $choice_input=<STDIN>;
+    chomp $choice_input;
+    if( $choice_input  ==  1 )
+    {
+        login();
+             
+   }elsif( $choice_input ==  2 )
+   {
+	register();
+	login();
+   }
+ }
+action_perform();
+
